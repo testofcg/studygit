c08635e3867dadbfa6e898883a561669655b7f0a
CGYOON <ckdrmsdl9999@naver.com>
2025-02-13 21:52:54 +0900

    init commit

---
+++ src/main/java/com/sparta/studygit1/Studygit1Application.java
@@ -0,0 +1,16 @@
+package com.sparta.studygit1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studygit1Application {
+
+    public static void main(String[] args) {
+        int num=1;
+
+
+        SpringApplication.run(Studygit1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/studygit1/Studygit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.studygit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studygit1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
